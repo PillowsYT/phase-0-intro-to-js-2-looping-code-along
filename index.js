5ec2980d263c40c443abe1b16976b30f8880214d
Maple <dinzobros@gmail.com>
2022-07-20 13:23:59 -0400

    commit tree 0dff638d8234d51bc7135160d3c1a5d9fa6854a2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const name = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(name){
+    for (let i = 0; i <name.length; i++) {
+        console.log(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+        debugger;
+    }
+
+    return name;
+}
+
+writeCards(name);
+
+let countDown = 0;
+while (countDown < 10) {
+  console.log(countDown++);
+}
